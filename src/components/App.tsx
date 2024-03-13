f2f8db9f90d44ab8cfad499c05f8fe0b2ecb94cf
Victor Maznichenko <66438596+Victor-Maznichenko@users.noreply.github.com>
2024-03-13 14:34:53 +0300

    🔨 Added implementation of infinite scroll with react-intersection-observer library

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import ModalCart from "@components/Cart/ModalCart";
 import Footer from "@components/Footer";
 import Header from "@components/Header";
-import Products from "@components/Products";
+import Products from "@components/Products/ProductsCopy";
 
 const App = () => (
   <div className="flex min-h-screen flex-col gap-y-px text-gray-main">
